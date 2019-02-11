f45a55a3d2c31aadd64f0a0b3f887219b8ae0dc8
Emma Calvet <emma.calvet.g@gmail.com>
2019-02-11 22:57:54 +0100

    ajout mailer SAMER

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,13 @@
 class Attendance < ApplicationRecord
+  after_create :new_attendee_send
+
   belongs_to :user
   belongs_to :event
+
+  private
+  
+  def new_attendee_send
+    AttendanceMailer.new_attendee_email(self).deliver_now
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
   has_many :attendances
   has_many :events, through: :attendances
   has_many :created_events, class_name: "Event", foreign_key: "admin_id"
+
+  private
+  
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
