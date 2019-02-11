05a1bef48ffc2f669261d6026c28702ddc3fb4b8
Emma Calvet <emma.calvet.g@gmail.com>
2019-02-11 23:14:28 +0100

    bug???

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,16 @@ puts "Création d'utilisateurs"
 10.times do 
   user = User.create!(email: Faker::Internet.free_email, description: Faker::Movies::HarryPotter.quote, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
-puts "Utilisateurs crée !"
+puts "Utilisateurs créés !"
 
 puts "Création d'events"
 10.times do 
-  event = Event.create!(start_date: Faker::Date.forward, duration: 50, title: Faker::Book.title, description: Faker::Hacker.say_something_smart , price: Faker::Number.between(1, 1000)  , location: Faker::TvShows::GameOfThrones.city, admin_id: rand(User.all.first.id..User.all.last.id))
+  event = Event.create!(start_date: Faker::Date.forward, duration: 50, title: Faker::Book.title, description: Faker::Hacker.say_something_smart , price: Faker::Number.between(1, 1000), location: Faker::TvShows::GameOfThrones.city, admin_id: rand(User.all.first.id..User.all.last.id))
 end
-puts "Events crée !"
+puts "Events créés !"
 
 puts "Création de reservation"
 10.times do 
-  resa = Attendance.create!(stripe_customer_id: Faker::Number.number(10), event_id: rand(Event.all.first.id..Event.all.last.id), user_id: rand(User.all.first.id..User.all.last.id))
+resa = Attendance.create!(stripe_customer_id: Faker::Number.number(10), event_id: rand(Event.all.first.id..Event.all.last.id), user_id: rand(User.all.first.id..User.all.last.id))
 end
-puts "Reservation crée !"
+puts "Reservation créée !"
