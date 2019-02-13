4a3b1f8e7ad49a8e346675458a73e7b3b2a5fc5f
Jimmy Charpagne <jimmy.charpagne@gmail.com>
2019-02-13 04:26:50 +0100

    encore plus de view

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -4,7 +4,7 @@ class EventController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
+    #@event = Event.find(params[:id])
   end
 
   def new
@@ -12,7 +12,7 @@ class EventController < ApplicationController
   end
 
   def edit
-    @event = Event.find(params[:id])
+    #@event = Event.find(params[:id])
   end
 
   def create
