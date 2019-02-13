2eef5a93acab94851530c0930705f4fabbf874c3
Emma Calvet <emma.calvet.g@gmail.com>
2019-02-13 16:03:40 +0100

    on avance les gars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
+  before_action :own_profile, only: [:show]
   
   def show
   end
@@ -7,6 +8,10 @@ class UsersController < ApplicationController
   private
 
   def own_profile
-    
+    if params[:id].to_i == current_user.id
+    else
+      redirect_to user_path(current_user.id)
+      #flash[:warning] = "Tu n'as pas l'autorisation"  fix si on a le temps
+    end
   end
 end
