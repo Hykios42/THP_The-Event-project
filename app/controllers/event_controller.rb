ada288cf607568a8dd4537e9f2d8e41e1fe25cd3
Jimmy Charpagne <jimmy.charpagne@gmail.com>
2019-02-13 04:17:42 +0100

    On essais de faire les events

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,43 @@
+class EventController < ApplicationController
+  def index
+    @event = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(Event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+=begin  
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+=end
+end
